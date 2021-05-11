fad76fbb4a39b5d8bd47b786b917d434bb082073
andrejensigner <andrej.ensigner@gmail.com>
2021-05-11 21:57:28 +0200

    > RELEASE 11512
    - New starting script for FiveM (shell)
    - New bordeless moving script (now working)
    
    - Added Social Media buttons (FB, GIT)
    - Added Minimize button (working)
    - Added priority set timer (20s)
    
    - Fixed High Perf. Mode (now working)
    - Fixed process3 priority (FiveM_Chromebrowser)
    
    - Changed Launcher Design (better now)

---
+++ QuickM - New Version Build on 3.1/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace QuickM___New_Version_Build_on_3._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.start_button = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.facebook = new System.Windows.Forms.Button();
+            this.instagram = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start_button
+            // 
+            this.start_button.BackColor = System.Drawing.Color.Transparent;
+            this.start_button.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.start_button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.start_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.start_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.start_button.Font = new System.Drawing.Font("TitlingGothicFB Cond Bold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.start_button.ForeColor = System.Drawing.Color.White;
+            this.start_button.Location = new System.Drawing.Point(322, 290);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(170, 59);
+            this.start_button.TabIndex = 0;
+            this.start_button.Text = "START";
+            this.start_button.UseVisualStyleBackColor = false;
+            this.start_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::QuickM___New_Version_Build_on_3._1.Properties.Resources.quickmenu_logologo;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(205, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(414, 148);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Firebrick;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("TitlingGothicFB Cond Bold", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(775, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 25);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("SF UI Display", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(2, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "QuickMenu Launcher v1.0.4";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.checkBox1.Location = new System.Drawing.Point(329, 363);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(157, 19);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "High Performance Mode";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(621, 412);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 30);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "            QuickMenu release 11521 \r\nMade by Andrej Ensigner © 2021 ";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("TitlingGothicFB Cond Bold", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(750, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 25);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "_";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Vogue", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(-106, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1145, 11);
+            this.label3.TabIndex = 8;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // facebook
+            // 
+            this.facebook.BackColor = System.Drawing.Color.Transparent;
+            this.facebook.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.facebook.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.facebook.Font = new System.Drawing.Font("SF UI Display", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.facebook.ForeColor = System.Drawing.SystemColors.Control;
+            this.facebook.Location = new System.Drawing.Point(12, 405);
+            this.facebook.Name = "facebook";
+            this.facebook.Size = new System.Drawing.Size(30, 30);
+            this.facebook.TabIndex = 9;
+            this.facebook.Text = "FB";
+            this.facebook.UseVisualStyleBackColor = false;
+            this.facebook.Click += new System.EventHandler(this.facebook_Click);
+            // 
+            // instagram
+            // 
+            this.instagram.BackColor = System.Drawing.Color.Transparent;
+            this.instagram.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.instagram.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.instagram.Font = new System.Drawing.Font("SF UI Display", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.instagram.ForeColor = System.Drawing.SystemColors.Control;
+            this.instagram.Location = new System.Drawing.Point(48, 405);
+            this.instagram.Name = "instagram";
+            this.instagram.Size = new System.Drawing.Size(30, 30);
+            this.instagram.TabIndex = 10;
+            this.instagram.Text = "IG";
+            this.instagram.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::QuickM___New_Version_Build_on_3._1.Properties.Resources.wallhaven_yjr2z7;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.instagram);
+            this.Controls.Add(this.facebook);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button facebook;
+        private System.Windows.Forms.Button instagram;
+    }
+}
+
---
+++ QuickM - New Version Build on 3.1/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+
+namespace QuickM___New_Version_Build_on_3._1
+{
+    public partial class Form1 : Form
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        //--> Posúvanie Window ---------------------------------------------------
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_NCHITTEST)
+                m.Result = (IntPtr)(HT_CAPTION);
+        }
+
+        private const int WM_NCHITTEST = 0x84;
+        private const int HT_CLIENT = 0x1;
+        private const int HT_CAPTION = 0x2; 
+
+
+        // --> START BTN ---------------------------------------------------
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", "fivem://"); //Nové spúštanie FiveM ako Shell
+            
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Waiting for FiveM started!"); // Console Oznámenie
+                Thread.Sleep(10000); // <= Časovanie / potom spustí zostávajuce scripty (10000 = 20sekund)
+            }
+
+            Process prs = Process.GetCurrentProcess(); 
+            try
+            {
+                prs.MinWorkingSet = (IntPtr)(300000); 
+            }
+            catch (Exception)
+            {
+                throw new Exception();
+            }
+
+            
+            string appData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData); // Upresnuje %localappdata%
+            //System.Diagnostics.Process.Start(System.IO.Path.Combine(appData, @"FiveM\FiveM.exe")); // Staré spúštanie FiveM
+            Process[] processes = Process.GetProcessesByName("FiveM");
+            
+
+            foreach (Process proc in processes)
+            proc.PriorityClass = ProcessPriorityClass.RealTime; // High; AboveHigh;
+            
+
+            Process[] processes2 = Process.GetProcessesByName("FiveM_GTAProcess");
+            foreach (Process proc2 in processes2)
+                proc2.PriorityClass = ProcessPriorityClass.RealTime;
+
+            Process[] processes3 = Process.GetProcessesByName("FiveM_ChromeBrowser");
+            foreach (Process proc3 in processes3)
+                proc3.PriorityClass = ProcessPriorityClass.AboveNormal;
+        }
+
+        // --> CLOSE BTN ---------------------------------------------------
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Process[] Processes = Process.GetProcessesByName("explorer");
+            if (Processes.Length == 0)
+            {
+                string ExplorerShell = string.Format("{0}\\{1}", Environment.GetEnvironmentVariable("WINDIR"), "explorer.exe");
+                System.Diagnostics.Process prcExplorerShell = new System.Diagnostics.Process();
+                prcExplorerShell.StartInfo.FileName = ExplorerShell;
+                prcExplorerShell.StartInfo.UseShellExecute = true;
+                prcExplorerShell.Start();
+            }
+            System.Diagnostics.Process.Start("cmd.exe", "/c taskkill /F /IM FiveM.exe /T");
+            Application.Exit();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                checkBox1.Text = "High Performance Mode";
+                Process.Start(@"C:\Windows\System32\taskkill.exe", @"/F /IM explorer.exe");
+                MessageBox.Show("High Performance mode Started!");
+
+
+            }
+            else
+            {
+                checkBox1.Text = "High Performance Mode";
+                Process.Start(@"C:\Windows\explorer.exe");
+                MessageBox.Show("High Performance mode Disabled!");
+            }
+        }
+
+        // --> MINIMIZE BTN ---------------------------------------------------
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        // -- > MOUSE EVENTS !!! ----------------
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void facebook_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", "https://www.facebook.com/cloudensigner");
+        }
+        // --> 
+    }
+
+}
